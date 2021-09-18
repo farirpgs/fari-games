4c39f22677f1e71e35cba3ec222f4e188f88ce1a
rpdeshaies <rpdeshaies@gmail.com>
2021-09-18 18:40:48 -0400

    doc: homepage

--- data/shop/shopCategories.ts
+++ data/shop/shopCategories.ts
@@ -1,6 +1,4 @@
 export const shopCategories: Array<{ name: string; tags: string }> = [
   { name: "Checkout those great SRDs", tags: "main" },
-  // { name: "You will LOVE those games", tags: "staff-pick" },
-  // { name: "Fate ", tags: "fate" },
-  // { name: "Forged in the Dark", tags: "fitd" },
+  { name: "Master your Fate ", tags: "fate" },
 ];
