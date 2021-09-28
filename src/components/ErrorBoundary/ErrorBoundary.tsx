832e5de67bc626915bbc4a3d2b8bc8650255e1ce
rpdeshaies <rpdeshaies@gmail.com>
2021-09-28 08:57:19 -0400

    fix: tsc

--- src/components/ErrorBoundary/ErrorBoundary.tsx
+++ src/components/ErrorBoundary/ErrorBoundary.tsx
@@ -8,11 +8,13 @@ export class ErrorBoundary extends React.Component<{}, { hasError: boolean }> {
     this.state = { hasError: false };
   }
 
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   static getDerivedStateFromError(error: unknown) {
     // Update state so the next render will show the fallback UI.
     return { hasError: true };
   }
 
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   componentDidCatch(error: unknown, errorInfo: unknown) {
     // You can also log the error to an error reporting service
   }
