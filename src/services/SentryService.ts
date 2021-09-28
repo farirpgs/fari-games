6c6be8844dc13c02c2a8bfd4456536e8b58204e1
rpdeshaies <rpdeshaies@gmail.com>
2021-09-28 09:14:59 -0400

    feat: sentry

--- src/services/SentryService.ts
+++ src/services/SentryService.ts
@@ -1,19 +1,19 @@
-// import * as Sentry from "@sentry/react";
-// import { Integrations } from "@sentry/tracing";
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
 
 export const SentryService = {
   init() {
     if (import.meta.env.DEV) {
       return;
     }
-    // Sentry.init({
-    //   dsn: "https://45a9554609a0460899b2739c8e2c875d@o332302.ingest.sentry.io/5982870",
-    //   integrations: [new Integrations.BrowserTracing()],
+    Sentry.init({
+      dsn: "https://45a9554609a0460899b2739c8e2c875d@o332302.ingest.sentry.io/5982870",
+      integrations: [new Integrations.BrowserTracing()],
 
-    //   // Set tracesSampleRate to 1.0 to capture 100%
-    //   // of transactions for performance monitoring.
-    //   // We recommend adjusting this value in production
-    //   tracesSampleRate: 1.0,
-    // });
+      // Set tracesSampleRate to 1.0 to capture 100%
+      // of transactions for performance monitoring.
+      // We recommend adjusting this value in production
+      tracesSampleRate: 1.0,
+    });
   },
 };
