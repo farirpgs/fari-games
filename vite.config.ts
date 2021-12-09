874ccb32736554a1357e2472d1d0c9a5a3f938ab
rpdeshaies <rpdeshaies@gmail.com>
2021-12-09 11:48:02 -0500

    feat: vite reload

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,28 @@
-import reactRefresh from "@vitejs/plugin-react-refresh";
-import legacy from "@vitejs/plugin-legacy";
-import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { defineConfig, Plugin } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()],
+  plugins: [react(), reloadOnMarkdownChange()],
 
   server: {
     open: true,
     port: 1235,
   },
 });
+
+// TODO: move back to using `import("/public/documents/*/*.md?raw"),` when this issue is done https://github.com/vitejs/vite/issues/3222#issuecomment-917617647
+function reloadOnMarkdownChange() {
+  const plugin: Plugin = {
+    name: "reload",
+    handleHotUpdate({ file, server }) {
+      if (file.endsWith(".md")) {
+        server.ws.send({
+          type: "full-reload",
+          path: "*",
+        });
+      }
+    },
+  };
+  return plugin;
+}
