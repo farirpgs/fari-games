bda7b9180eaf3b926de97a0e3e46c1bc3864cff1
René-Pier Deshaies-Gélinas <renepier.dg@unity3d.com>
2022-07-12 09:00:38 -0400

    doc: personae SRD

--- data/shop/shopProducts.ts
+++ data/shop/shopProducts.ts
@@ -709,6 +709,25 @@ export const shop: IShop = {
         },
       ],
     },
+    {
+      name: "Craig Hatler",
+      slug: "craig-hatler",
+      links: {
+        twitter: "https://twitter.com/tezrak",
+      },
+      products: [
+        {
+          name: "Personae SRD",
+          slug: "personae-srd",
+          description:
+            "Build any kind of characters you can imagine, and allow for meaningful choices to impact the unfolding of thrilling adventures.",
+          image: "https://gyazo.com/fa1ea25d2e44a6911ccfe849be8a2a51.png",
+          links: {},
+          tags: ["srd"],
+          type: ProductTypeEnum.SRD,
+        },
+      ],
+    },
   ],
 };
 
